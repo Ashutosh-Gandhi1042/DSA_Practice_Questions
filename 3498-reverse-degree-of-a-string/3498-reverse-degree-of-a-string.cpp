4b2890f3880605ec0271fa4a17cb3f0c5d0be340
Ashutosh Gandhi <ashutoshgandhi10423@gmail.com>
2026-09-22 13:49:10 +0530

    Time: 0 ms (100%), Space: 9.6 MB (92.66%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int res=0;
+        for(int i=0;i<s.size();i++)
+        {
+            res+=('z'-s[i]+1)*(i+1);
+        }
+        return res;    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
